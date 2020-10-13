6f2b5a5817ffec7438d5d8880c8aa1b7faa51847
SAILOKESHREDDY <sailokeshreddy6@gmail.com>
2020-10-13 13:13:05 +0530

    calc prog

--- Hello.py
+++ Hello.py
@@ -1,5 +1,15 @@
+from calculator import calculator
+
 class Hello:
     @staticmethod
     def hello():
         print("Hello World")
+
 Hello.hello()
+
+print(calculator.add(5,6))
+print(calculator.sub(15,3))
+print(calculator.mul(20,25))
+print(calculator.div(100,5))
+
+print('END')
---
+++ calculator.py
@@ -0,0 +1,17 @@
+class calculator:
+
+	@staticmethod
+	def add(a,b):
+		return a+b
+
+	@staticmethod
+	def sub(a,b):
+		return abs(a-b)
+
+	@staticmethod
+	def mul(a,b):
+		return a*b
+
+	@staticmethod
+	def div(a,b):
+		return a/b
