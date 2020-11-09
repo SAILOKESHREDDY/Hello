b07438100a3a18ff59ac6153d40e8609b1f661c7
SAILOKESHREDDY <41065679+SAILOKESHREDDY@users.noreply.github.com>
2020-11-09 13:07:37 +0530

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -7,10 +7,10 @@ class Hello:
 
 Hello.hello()
 
-assert(calculator.add(5,6))
-assert(calculator.sub(15,3))
-assert(calculator.mul(20,25))
-assert(calculator.div(100,5))
-assert(calculator.mod(4,6))
+assert(calculator.add(5,6)==11)
+assert(calculator.sub(15,3)==12)
+assert(calculator.mul(20,25)==450)
+assert(calculator.div(100,5)==20)
+assert(calculator.mod(4,6)==4)
 
 print('END')
