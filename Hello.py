6b43e3f1eec0b7e3a214092c1455e84c3c43d0d6
SAILOKESHREDDY <41065679+SAILOKESHREDDY@users.noreply.github.com>
2020-11-10 01:50:13 +0530

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -9,26 +9,31 @@ Hello.hello()
 
 try:
     assert(calculator.add(5,6)==11)
+    print("1")
 except:
     print("ADD function failed")
 
 try:
     assert(calculator.sub(15,3)==12)
+    print("2")
 except:
     print("SUB function failed")
 
 try:
     assert(calculator.mul(20,20)==400)
+    print("3")
 except:
     print("MUL function failed")
 
 try:
     assert(calculator.div(100,5)==20)
+    print("4")
 except:
     print("DIV function failed")
 
 try:
     assert(calculator.mod(4,6)==4)
+    print("5")
 except:
     print("MOD function failed")
 
