d74fc618e7a2be590af28f4166610be9c4972feb
SAILOKESHREDDY <41065679+SAILOKESHREDDY@users.noreply.github.com>
2020-11-09 13:15:58 +0530

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -7,10 +7,29 @@ class Hello:
 
 Hello.hello()
 
-assert(calculator.add(5,6)==11)
-assert(calculator.sub(15,3)==12)
-assert(calculator.mul(20,25)==450)
-assert(calculator.div(100,5)==20)
-assert(calculator.mod(4,6)==4)
+try:
+    assert(calculator.add(5,6)==11)
+except:
+    print("ADD function failed")
+
+try:
+    assert(calculator.sub(15,3)==12)
+except:
+    print("SUB function failed")
+
+try:
+    assert(calculator.mul(20,20)==400)
+except:
+    print("MUL function failed")
+
+try:
+    assert(calculator.div(100,5)==20)
+except:
+    print("DIV function failed")
+
+try:
+    assert(calculator.mod(4,6)==4)
+except:
+    print("MOD function failed")
 
 print('END')
