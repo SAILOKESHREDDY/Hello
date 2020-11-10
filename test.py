b8b24405245da4d4874eb2f45f09e8822c00b45a
SAILOKESHREDDY <41065679+SAILOKESHREDDY@users.noreply.github.com>
2020-11-10 13:01:48 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,13 @@
+import unittest
+from calculator import calculator
+
+class test(unittest.TestCase):
+    @staticmethod
+    def test(self):
+	    self.assertEqual(calculator.add(5,6)==11)
+	    self.assertEqual(calculator.sub(15,3)==12)
+	    self.assertEqual(calculator.mul(20,20)==400)
+	    self.assertEqual(calculator.div(100,5)==20)
+	    self.assertEqual(calculator.mod(4,6)==4)
+
+unittest.main()
